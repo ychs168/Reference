3965df4ce8ec9de065185e6c54e245cab19ae675
英才汇硕 <30660292+ychs168@users.noreply.github.com>
2017-08-12 19:44:31 +0800

    四则运算类

---
+++ JUnit/Calc.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2017, 2018, YCHS-英才汇硕信息科技有限公司. All rights reserved.
+ */
+package com.ychs.java;
+
+/**
+ * 完成两个double型数字的计算
+ * 
+ * @author wuzhijun
+ * @version 1.0  
+ */
+public class Calc {
+	/**
+	 * 完成加法计算
+	 * 
+	 * @param a
+	 *            第一个操作数
+	 * @param b
+	 *            第二个操作数
+	 * @return 加法计算得到的结果
+	 */
+	public double add(int a, int b) {
+		return a + b;
+	}
+
+	/**
+	 * 完成减法计算
+	 * 
+	 * @param a
+	 *            第一个操作数
+	 * @param b
+	 *            第二个操作数
+	 * @return 减法计算得到的结果
+	 */
+	public double sub(int a, int b) {
+		return a - b;
+	}
+
+	/**
+	 * 完成乘法计算
+	 * 
+	 * @param a
+	 *            第一个操作数
+	 * @param b
+	 *            第二个操作数
+	 * @return 乘法计算得到的结果
+	 */
+	public double mul(int a, int b) {
+		return a * b;
+	}
+
+	/**
+	 * 完成除法计算
+	 * 
+	 * @param a
+	 *            第一个操作数
+	 * @param b
+	 *            第二个操作数
+	 * @return 除法计算得到的结果
+	 */
+	public double div(int a, int b) {
+		return a / b;
+	}
+
+}
